100dc823cf3873cd7133f9f5e74d2b074949c593
Nguyen Pham Tuyen <phamtuyen121314@gmail.com>
2024-11-05 16:52:41 +0700

    exception yt

--- studyYT/identity-service/src/main/java/com/famtwen/identity_service/repository/UserRepository.java
+++ studyYT/identity-service/src/main/java/com/famtwen/identity_service/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.famtwen.identity_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    boolean existsByUsername(String username);
 }
